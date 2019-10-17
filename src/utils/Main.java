a1ded8138b54e0fedd96df1f5780c92f226a1a52
Dida06280 <56685121+Dida06280@users.noreply.github.com>
2019-10-17 17:14:39 +0300

    Am mai facut ceva

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,5 +1,29 @@
 package utils;
-
-public class Main {
-
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String args [])
+    {
+    	Scanner sc=new Scanner (System.in);
+    	System.out.println("Introdu nota");
+    	Integer note =sc.nextInt();
+    	System.out.println("Alegeti subiect: \n 1. -DE \n 2. -SS \n 3. -Engleza \n 4. -MS");
+    	Integer options=sc.nextInt();
+    	switch (options) 
+    	{
+    	case 1: 
+    		System.out.println("Nota la DE este " +note);
+    		break;
+    	case 2:	
+    		System.out.println("Nota la SS este " +note);
+    		break;
+    	case 3:	
+    		System.out.println("Nota la Engleza este " +note);
+    		break;
+    	case 4:	
+    		System.out.println("Nota la Ms este " +note);
+    		break;
+    	}
+    
+    }
 }
